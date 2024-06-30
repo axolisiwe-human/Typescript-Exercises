c2568746aeba50117914dcb908b0c43b043d3094
Human <humanbakishi@gmail.com>
2024-06-30 19:38:11 +0200

    Third update

--- exercise4.ts
+++ exercise4.ts
@@ -1,26 +1,26 @@
 //Create a Person class with name and age properties. Implement a method to return
 // the person's name.
 class Person{
-    name:string 
-    age:number
+    private name:string 
+    private age:number
+    private socialSecurityNumber:number
 
     constructor (nam:string, ag:number){
         this.name = nam
         this.age = ag
     }
+    func_name = (nam:string) => {
+        nam = "Human Bakishi"
+        accessor.name = nam
+        return accessor.name
+    }
+    func_age = (ag:number) => {
+        ag = 24
+        accessor.age = ag
+        return accessor.age
+    }
 }
 let accessor = new Person("Name",0)
 
-let func_name = (nam:string) => {
-    nam = "Human Bakishi"
-    accessor.name = nam
-    return accessor.name
-}
-let func_age = (ag:number) => {
-    ag = 24
-    accessor.age = ag
-    return accessor.age
-}
-
-console.log(func_name(""))
-console.log(func_age(0))
+console.log(accessor.func_name(""))
+console.log(accessor.func_age(0))
